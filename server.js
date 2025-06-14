115bb458d2959961a40b0653098d31b3cba5ee94
Agniva-07 <haitagniva@gmail.com>
2025-06-14 12:12:25 +0530

    allowed all cors

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ const allowedOrigins = [
 ];
 
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(cors({ origin: '*' }));
 app.use(express.json())
 app.use(express.static("public"))
 
